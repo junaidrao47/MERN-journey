b94b4f69c78560067215a1f759c17ed04e073df1
JUNAID RAO <mjprods47@gmail.com>
2024-05-02 01:34:05 +0500

    DOM

---
+++ Javascript/DOM/script1.js
@@ -0,0 +1,6 @@
+let value = document.getElementById("input").value;
+document.getElementById("input2").click();
+document.querySelector("a").style.color="RED";
+document.querySelector("a").style.backgroundColor="BLUE";
+document.getElementById("h1").style.color = "Red";
+console.log("value");
